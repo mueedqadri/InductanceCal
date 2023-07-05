4c108e325e02210e4303c0c7334809fae185bc37
sheikhsoliha <sheikhsoliha@sheikhs-air>
2023-07-04 23:18:20 -0400

    NEW CHANGE

--- try2current.m
+++ try2current.m
@@ -2,47 +2,55 @@ clc;
 clear all;
 
 
+%As a general rule, your dt should be small enough that the variables you're simulating do not change drastically within each time step.
+
 dt = 0.0001; % time step
-tend = 0.7; % end time
+tend = 20; % end time
 t= 0:dt:tend;
 [n, size_t] = size(t);
 % 1. Specify input variables: Va(t), Vb(t), Vc(t), and TL(t)
-f=50;
+f=60;
 TL = 0; % replace with the actual function
 % 2. Specify initial conditions: θ0, ω0, t0=0, [ψs]0, [ψr]0
 phis  = zeros(3,1,size_t); % initial psi_s
 phir = zeros(3,1,size_t); % initial psi_r
 Labcsr =  zeros(3,3,size_t);
 Labcrs =  zeros(3,3,size_t);
-R3 = zeros(3,3);
 Ir = zeros(3,1, size_t);
 Is = zeros(3,1, size_t);
 Te = zeros(1,size_t);
 Va = zeros(1,size_t);
 Vb = zeros(1,size_t);
 Vc = zeros(1,size_t);
-Vs = zeros(3,1, size_t);
+Vs = zeros(3,1,size_t);
 %5. Specify constants
-Rs = 0.435;
-Rr = 0.816;
-Xls = 0.754;
-Xm = 26.13;
-Lls = (Xls/(2*pi*f));
-Llr = Lls;
-Lms = (Xm/(2*pi*f));
-Lmr = Lms;
+Rs = 0.169;
+Rr = 0.116;
+% Xls = 0.754;
+% Xm =26.13;
+% Xlr =0.754;
+% Lls = Xls/(2*pi*f);
+% Llr = Xlr/(2*pi*f);
+% Lms = Xm/(2*pi*f);
+% Lmr = Lms;
+Lls = 3.73e-4;
+Llr = 1.05e-3;
+Lm = 3.27e-3;
+Lms =(2/3)*Lm;
 P = 4;
 B=0;
-J=0.089;
+J=0.0889;
+Vm = 29.39;
+N = 0.29;
 R1 = [Rs 0 0; 0 Rs 0; 0 0 Rs];
 R2 = [Rr 0 0; 0 Rr 0; 0 0 Rr];
 Labcs = [Lls+Lms,                  -0.5*Lms,                  -0.5*Lms;
-    -0.5*Lms,                    Lls+Lms,                  -0.5*Lms;
-    -0.5*Lms,                  -0.5*Lms,                   Lls+Lms ];
+       -0.5*Lms,                    Lls+Lms,                  -0.5*Lms;
+       -0.5*Lms,                  -0.5*Lms,                   Lls+Lms ];
 
-Labcr = [Llr+Lmr,                  -0.5*Lmr,                  -0.5*Lmr;
-    -0.5*Lmr,                    Llr+Lmr,                  -0.5*Lmr;
-    -0.5*Lmr,                  -0.5*Lmr,                   Llr+Lmr ];
+Labcr = [Llr+Lms,                  -0.5*Lms,                  -0.5*Lms;
+    -0.5*Lms,                    Llr+Lms,                  -0.5*Lms;
+    -0.5*Lms,                  -0.5*Lms,                   Llr+Lms ];
 
 
 % 3. Specify simulation time step and end: Δt, tend
@@ -62,28 +70,29 @@ for i = 1: size_t
             -Lms*sin(theta(i-1)+(2*pi/3)),   -Lms*sin(theta(i-1)-(2*pi/3)),    -Lms*sin(theta(i-1))];
         Labcrsdiff =  (Labcsrdiff)';
         % 6. Calculate current vectors [Is]i-1 and [Ir]i-1 using (23) and (24)
-        Ir(:, : , i-1)= (inv(Labcr-(Labcrs*inv(Labcs)*Labcsr)))*(phir(:,:,i-1) - (Labcrs*inv(Labcs)*phis(:,:,i-1)));
-        Is(:, :, i-1) = (inv(Labcs)*phis(:,:,i-1))-(inv(Labcs)*Labcsr*Ir(:,:,i-1));
+         Ir(:, : , i-1)= ((inv(Labcr-((Labcsr)'*inv(Labcs)*Labcsr)))*(N*phir(:,:,i-1) - ((Labcsr)'*inv(Labcs)*phis(:,:,i-1))));
+         % Ir(:, : , i-1) = inv(Labcrs*Labcsr-Labcr)*(((Labcrs*inv(Labcs)*phis(:,:,i-1))-phir(:,:,i-1));
+         Is(:, :, i-1) = (inv(Labcs)*phis(:,:,i-1))-(inv(Labcs)*Labcsr*Ir(:,:,i-1));
         
         % 7. Calculate induced torque Te,i-1
-        % Te(i-1) = (0.5*(Is(:,:,i-1))'* Labcsrdiff*(Ir(:,:,i-1)))+(0.5*(Ir(:,:,i-1))'* Labcrsdiff*(Is(:,:,i-1)));
-        Te(i-1) = (0.5*(Is(:,:,i-1))'* Labcsrdiff*(Ir(:,:,i-1)));
-        %12. Calculate Voltges
-        Va(i) = 230*sqrt(2)*sin(2*pi*f*t(i)); % replace with the actual function
-        Vb(i) = 230*sqrt(2)*sin(2*pi*f*t(i)-(120*pi/180)); % replace with the actual function
-        Vc(i) = 230*sqrt(2)*sin(2*pi*f*t(i)+(120*pi/180));
+        % Te(i-1) = (((0.5*(Is(:,:,i-1))'* Labcsrdiff*(Ir(:,:,i-1)))+(0.5*(Ir(:,:,i-1))'* Labcrsdiff*(Is(:,:,i-1)))));
+         Te(i-1) = (P/2)*((Is(:,:,i-1))'* Labcsrdiff*(Ir(:,:,i-1)));
+        % %12. Calculate Voltges
+        Va(i) = Vm*sin(2*pi*f*t(i)); % replace with the actual function
+        Vb(i) = Vm*sin(2*pi*f*t(i)-(120*pi/180)); % replace with the actual function
+        Vc(i) = Vm*sin(2*pi*f*t(i)+(120*pi/180));
         Vs(:, :, i) = [Va(i);Vb(i);Vc(i)];
         % 8. Calculate flux linkage vectors [ψs]i and [ψr]i using (21) and (22)
-        phis(:,:,i) = ((Vs(:, :, i)-(R1*Is(:,:,i-1)))*dt) + phis(:,:,i-1);
-        phir(:,:,i)= (-(R1*Ir(:,:,i-1))*dt) + phir(:,:,i-1);
+        phis(:,:,i) = ((Vs(:, :, i-1)-(R1*Is(:,:,i-1))))*dt + phis(:,:,i-1);
+        phir(:,:,i)= (-(R2*Ir(:,:,i-1))*dt*(1/N)) + phir(:,:,i-1);
         
         % 9. Calculate rotor angular speed ωi and position θi using (25) and 26
-        omega(i) = (((((1/J)*Te(i-1)- TL)-((B/J)*omega(i)))*dt)*(P/2)) + omega(i-1);
+        omega(i) = ((((((1/(J))*Te(i-1)- TL))*dt*(P/2))) + omega(i-1));
         % omega(i) = (1/J*(Te(i-1)-TL-((B/J)*(omega(i)))))*dt + omega(i-1);
-        theta(i) = (omega(i)*dt)+ theta(i-1);
+        theta(i) = ((omega(i)*dt)+ theta(i-1));
         
         % 10. ti=ti-1+Δt
-        t(i) = t(i-1) + dt;
+        % t(i) = t(i-1) + dt;
     end
 end
 
@@ -93,24 +102,26 @@ Ir_A = squeeze(I(1,1,:));
 Ir_B = squeeze(I(2,1,:));
 Ir_C = squeeze(I(3,1,:));
 
-%Create a time vector to represent the time steps at which each current reading was taken
+% Create a time vector to represent the time steps at which each current reading was taken
 time_vector = dt:dt:size_t*dt;
 
 %Create the plot
 figure;
-plot(time_vector, Ir_A, 'r'); % Plot Phase A in red
-hold on;
-plot(time_vector, Ir_B, 'g'); % Plot Phase B in green
-plot(time_vector, Ir_C, 'b'); % Plot Phase C in blue
-hold off;
+plot(theta, Ir_A, 'r'); % Plot Phase A in red
+% hold on;
+% plot(time_vector, Ir_B, 'g'); % Plot Phase B in green
+% plot(time_vector, Ir_C, 'b'); % Plot Phase C in blue
+% hold off;
 %Label the plot
 title('Current Over Time for Each Phase');
 xlabel('Time (s)');
 ylabel('Current (A)');
 legend('Phase A', 'Phase B', 'Phase C');
+% ylim([-50,50])
 
-
-omega_2 = (omega*(60/(2*pi)))*(2/P);
-plot(t,Te,'red')
+omega_2 = (omega)*(2/P);
+    % plot(t,Te,'red')
+grid on
 % plot(omega_2,Te,'red')
-% plot(t,omega_2)
+   % plot(t,omega_2)
+% plot(t,theta)
